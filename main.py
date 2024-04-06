dec58f1d247f9a36c43c65c0588d3b2f88d1edbb
SungjunEom <junjun607@gmail.com>
2024-04-07 00:21:37 +0900

    Added error evaluation

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ import matplotlib.pyplot as plt
 np.random.seed(0)
 
 class System:
-    def __init__(self, loss, horizon, system, x0):
+    def __init__(self, loss, horizon, system, x0, xN):
         self.loss = loss
         self.horizon = horizon
-        self.inputs = np.random.random(horizon-1)
+        # self.inputs = np.random.random(horizon-1)
+        self.inputs = np.zeros(horizon-1)
         self.states = np.zeros(horizon)
         self.states[0] = x0
+        self.state_dest = xN
         self.delta_states = np.zeros(horizon)
         self.system = system
         self.sum_count = 0
 
     def get_states(self):
         return self.states
+    
+    def set_dest(self):
+        self.states[-1] = self.state_dest
 
     def diff_x(self, func, x0, u0, delta=1e-7):
         return (func(x0+delta, u0)-func(x0, u0))/delta
@@ -42,7 +47,8 @@ class System:
             self.states[i] = state
     
     def backward(self):
-        Vx = self.diff_x(self.loss,self.states[-1],0)
+        self.set_dest()
+        Vx = self.diff_x(self.loss, self.states[-1], 0)
         Vxx = self.diff_xx(self.loss, self.states[-1],0)
         for i in range(len(self.states)-2, -1, -1):
             Qx = self.diff_x(self.loss, self.states[i], self.inputs[i]) \
@@ -61,15 +67,17 @@ class System:
                 + Vx * self.diff_ux(self.system, self.states[i], self.inputs[i])
             Quu = self.diff_uu(self.loss, self.states[i], self.inputs[i]) \
                 + self.diff_u(self.system, self.states[i], self.inputs[i])**2 \
+                * Vxx \
                 + Vx * self.diff_uu(self.system, self.states[i], self.inputs[i])
             
-            k = -Qu/(Quu+1e-7)
-            K = -Qux/(Quu+1e-7)
+            k = -Qu/Quu
+            K = -Qux/Quu
             Vx = Qx - K*Quu*k
             Vxx = Qxx - K*Quu*K
 
             self.inputs[i] = self.inputs[i] + (k + K * self.delta_states[i])
 
+    # Auxiliaries
     def summary(self):
         self.sum_count += 1
         print()
@@ -77,7 +85,9 @@ class System:
         print('states:',self.states)
         print('inputs:',self.inputs)
         print('=======================')
-        print()
+
+    def error(self):
+        return self.states[-1] - self.state_dest
             
         
 
@@ -89,22 +99,14 @@ def system(x, u):
 
 
 if __name__ == "__main__":
-    wow = System(loss, 10, system, 0.2)
-    state1 = wow.get_states().copy()
-    wow.summary()
-    wow.forward()
-    wow.backward()
-    wow.summary()
-    state2 = wow.get_states().copy()
-    wow.forward()
-    wow.backward()
-    wow.summary()
-    state3 = wow.get_states().copy()
-    print(state1)
-    print(state2)
-    print(state3)
-    # plt.plot(state1)
-    # plt.plot(state2, 'bo')
-    # plt.plot(state3, 'r+')
-    # # plt.yscale('log', base=10)
-    # plt.show()
+    wow = System(loss, 10, system, 0.2, 0.3)
+    errors = []
+    for i in range(100):
+        wow.forward()
+        errors.append(wow.error())
+        wow.backward()
+    print(errors)
+    plt.plot(errors)
+    plt.ylabel('error of the final states')
+    plt.xlabel('iteration')
+    plt.show()
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 # Implementation of Differential Dynamic Programming
 ## Overview
-
+Work in progress.
+## Gallery
+![Errors](./Figure_1.png)
 ## References
 [Constrained Differential Dynamic Programming Revisited](https://arxiv.org/abs/2005.00985)
 
